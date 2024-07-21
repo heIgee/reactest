22c13ea026f32e41bc2296efbfe17519b6e9fd9a
heIgee <oleo7979@gmail.com>
2024-07-21 10:58:33 +0300

    separate dir for start playground

--- start/src/App.jsx
+++ start/src/App.jsx
@@ -9,14 +9,14 @@ import ColorGrid from './ColorGrid';
 import ScoreKeeper from './ScoreKeeper';
 import './App.css';
 
-// const properties = [
-//   { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
-//   { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
-//   { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
-//   { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
-//   { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
-//   { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
-// ];
+const properties = [
+  { id: 129031, name: 'Desert Yurt', rating: 4.9, price: 150 },
+  { id: 129331, name: 'Lone Mountain Cabin', rating: 4.8, price: 250 },
+  { id: 129032, name: 'Cactus Retreat', rating: 4.75, price: 300 },
+  { id: 129033, name: 'Redwood Treehouse Escape', rating: 4.9, price: 120 },
+  { id: 129034, name: 'Oceanview Condo', rating: 4.7, price: 140 },
+  { id: 129035, name: 'Gold Miner Campground', rating: 4.69, price: 96 },
+];
 
 // const colors = [
 //   "#E53935", "#E91E63", "#9C27B0", "#673AB7",
@@ -32,16 +32,15 @@ function App() {
 
   return (
     <>
+      <ColorGrid colors={colors} width='100%' height='100%' cols={44} rows={27} />
       <ScoreKeeper numPlayers={5} target={5} />
 
-      {/* <ColorGrid colors={colors} width="100%" height="100%" cols={44} rows={27} /> */}
+      <FaceCounter />
 
-      {/* <FaceCounter /> */}
+      <PropertyList properties={properties} />
+      <SlotMachine />
 
-      {/* <Clicker message="Goodbye!" content="Helo!" /> */}
-
-      {/* <PropertyList properties={properties} />
-      <SlotMachine /> */}
+      {/* <Clicker message='Goodbye!' content='Hello!' /> */}
 
       {/* <div >
         <a href="https://vitejs.dev" target="_blank">
@@ -68,3 +67,4 @@ function App() {
 }
 
 export default App;
+
